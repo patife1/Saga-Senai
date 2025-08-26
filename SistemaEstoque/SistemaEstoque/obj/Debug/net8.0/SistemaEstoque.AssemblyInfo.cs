2deb1ccfc3cbe5cd5e71bd0406ee1e2d5f7d1143
Izabelly Márcia <santosizabelly6@gmail.com>
2025-08-26 11:03:47 -0300

    Update

--- SistemaEstoque/SistemaEstoque/obj/Debug/net8.0/SistemaEstoque.AssemblyInfo.cs
+++ SistemaEstoque/SistemaEstoque/obj/Debug/net8.0/SistemaEstoque.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13802e8b3e960d0daf6f23f9836ee9c0ea088186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7dbede6b54f27adac664a856552a0177d1e83d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
